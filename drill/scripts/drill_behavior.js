667a6a7e7623e94852affbb6e80623be65d64151
Anna Springfield <anna.e.springfield@gmail.com>
2017-02-26 13:21:43 -0600

    create noneForUndefined

--- drill/scripts/drill_behavior.js
+++ drill/scripts/drill_behavior.js
@@ -35,9 +35,15 @@ $(document).ready(function() {
 var updateDrill = function() {
   $('#battery_amp_hours').text(drill1.batteryAmpHours);
   $('#amp_hours_left').text(drill1.ampHoursLeft.toFixed(1));
-  $('#current_bit').text(drill1.currentBit);
-  $('#current_speed').text(drill1.currentSpeed);
+  $('#current_bit').text(noneForUndefined(drill1.currentBit));
+  $('#current_speed').text(noneForUndefined(drill1.currentSpeed));
   $('#speeds').text(drill1.speeds.toSpaceString());
   $('#bits').text(drill1.bits.toSpaceString());
   $('#rotate_dir').text(drill1.rotateDir);
+};
+
+var noneForUndefined = function(property) {
+  if (property === undefined) {
+    return "None";
+  }
 };
